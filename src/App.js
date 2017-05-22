226eedf4db5a9200d214bb8b6d58077894a5a6a1
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-05-22 09:05:57 -0700

    Add and display raw pokemon data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Card } from 'antd';
+import { generation } from './pokemon';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-        <Card title="Card title" extra={<a href="#">More</a>} style={{ width: 300 }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+        <Card title={generation.pokemon_species[120].name} extra={<a href="#">1</a>} style={{ width: 300 }}>
+          <pre><code>{JSON.stringify(generation,null,2)}</code></pre>
         </Card>
     );
   }
