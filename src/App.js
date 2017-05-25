3c88fd6d89e07dfcc552ef81af32963659ed09ba
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-05-25 09:37:08 -0700

    Search complete

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { getSprite } from 'pokemon-images';
 import './App.css';
 import { generation } from './pokemon';
 import Tile from './Tile';
+import { Input } from 'antd';
+const Search = Input.Search;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonSpecies: generation.pokemon_species,
+      searchTerm: '',
+    };
+  }
   render() {
     const pokeCards = {
       display: 'flex',
@@ -14,19 +23,37 @@ class App extends React.Component {
     const tileStyle = {
       margin: '10px',
     }
+    // const filteredPokemonSpecies = this.state.pokemonSpecies
     return (
-      <div style={pokeCards}>
-        {
-          generation.pokemon_species.map((pokemon, i) => {
-            return (
-              <Tile
-                style={tileStyle}
-                name={pokemon.name}
-                image={getSprite(pokemon.name)}
-              />
-            )
-          })
-        }
+      <div>
+        <Search
+          placeholder={this.state.searchTerm}
+          style={{ width: 200, margin: '10px' }}
+          onSearch={value => this.setState({searchTerm: value})}
+          onBlur={ ({target}) => {
+            this.setState({searchTerm: target.value})
+          }}
+        />
+        <div>
+          {this.state.searchTerm}
+        </div>
+        <div style={pokeCards}>
+          {
+            this.state.pokemonSpecies
+            .filter((pokemon) => {
+              return pokemon.name.includes(this.state.searchTerm)
+            })
+            .map((pokemon, i) => {
+              return (
+                <Tile
+                  style={tileStyle}
+                  name={pokemon.name}
+                  image={getSprite(pokemon.name)}
+                  />
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
