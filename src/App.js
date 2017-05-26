349363dd8649111d3da1c100d838fe5c7f57f45c
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-05-26 08:47:01 -0700

    add search and filter functionality

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import { getSprite } from 'pokemon-images';
 import './App.css';
 import { generation } from './pokemon';
 import Tile from './Tile';
+import { Input } from 'antd';
+const Search = Input.Search;
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      searchTerm: '',
+    }
+  }
   render() {
     const pokeCards = {
       display: 'flex',
@@ -15,18 +23,28 @@ class App extends React.Component {
       margin: '10px',
     }
     return (
-      <div style={pokeCards}>
-        {
-          generation.pokemon_species.map((pokemon, i) => {
-            return (
-              <Tile
-                style={tileStyle}
-                name={pokemon.name}
-                image={getSprite(pokemon.name)}
-              />
-            )
-          })
-        }
+      <div>
+        <Search
+          placeholder={this.state.searchTerm}
+          style={{ width: 200 }}
+          onSearch={value => this.setState({searchTerm: value})}
+        />
+        <div style={pokeCards}>
+          {
+            generation.pokemon_species
+            .filter((pokemon) => {return pokemon.name.includes(this.state.searchTerm)})
+            .map((pokemon, i) => {
+              return (
+
+                <Tile
+                  style={tileStyle}
+                  name={pokemon.name}
+                  image={getSprite(pokemon.name)}
+                  />
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
